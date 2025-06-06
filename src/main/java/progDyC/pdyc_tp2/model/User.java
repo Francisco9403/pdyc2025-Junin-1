a91264bf6e7df0b39172a2134430f0e9f8a639e4
joaquinrios-unnoba <jrios912@comunidad.unnoba.edu.ar>
2025-06-06 20:26:57 -0300

    agregue los atributos pedidos al modelo User

--- src/main/java/progDyC/pdyc_tp2/model/User.java
+++ src/main/java/progDyC/pdyc_tp2/model/User.java
@@ -9,12 +9,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long  id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nombre;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    
     
     public Long getId() {
         return id;
     }
+
 
     public String getNombre() {
         return nombre;
@@ -22,6 +30,23 @@ public class User {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 
     
 }
